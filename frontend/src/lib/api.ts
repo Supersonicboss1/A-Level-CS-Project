3b697a242fc7239f4ce82412f088f113c181845b
Leo Dalgado-Molloy <leodalgado@gmail.com>
2023-05-11 22:25:32 +0000

    weird fix for local dev server

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,4 +1,4 @@
-const urlAPI = import.meta.env.VITE_isWeb ? `https://${import.meta.env.VITE_CSNAME}-8000.preview.app.github.dev` : 'http://localhost:8000';
+const urlAPI = import.meta.env.VITE_isWeb ? `https://${import.meta.env.VITE_CSNAME}-8000.preview.app.github.dev` : 'http://127.0.0.1:8000';
 import createClient from "openapi-fetch";
 import type { paths } from "./types/api";
 const { get, post } = createClient<paths>({ baseUrl: urlAPI });
