5082de4fc22ffafdd4a64144eae92e5062810923
Leo Dalgado-Molloy <leodalgado@gmail.com>
2023-05-11 08:26:55 +0000

    fix gitignore, add login page

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,15 +1,16 @@
-const isWeb = false
-export const urlAPI = isWeb ? `https://${import.meta.env.VITE_CSNAME}-8000.preview.app.github.dev/api/` : 'http://localhost:8000/api/';
+export const urlAPI = import.meta.env.VITE_isWeb ? `https://${import.meta.env.VITE_CSNAME}-8000.preview.app.github.dev/api/` : 'http://localhost:8000/api/';
+console.log(urlAPI);
+console.log(typeof import.meta.env.VITE_isWeb);
 export async function fetchBackend(route: string, data?: RequestInit) {
-		const x = urlAPI + route;
-		// check if any double slashes after https://
-		const y = x.replace(/([^:]\/)\/+/g, '$1');
-		return fetch(y, data);
-	}
-export async function fetchData(urlValue='/') {
-		const response = await fetchBackend(urlValue);
+	const x = urlAPI + route;
+	// check if any double slashes after https://
+	const y = x.replace(/([^:]\/)\/+/g, '$1');
+	return fetch(y, data);
+}
+export async function fetchData(urlValue = '/') {
+	const response = await fetchBackend(urlValue);
 
-		const data = await response.json();
-		console.log(data);
-		return data;
-	}
+	const data = await response.json();
+	console.log(data);
+	return data;
+}
--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -1,55 +1,87 @@
-<svelte:head>
-	<title>About</title>
-	<meta name="description" content="About this app" />
-</svelte:head>
 <script lang="ts">
-	import { fetchBackend, fetchData, urlAPI } from "$lib/api";
+	let signInResponse = '';
+	import { fetchBackend, fetchData } from '$lib/api';
+	import { token } from '$lib/stores';
 	let formData = {
 		username: 'test',
-		password: 'test',
-	}; 
+		password: 'test'
+	};
 	let isSignUp = false;
 	console.log(formData);
-	let showPassword = false;
-	function submitForm() {
+	async function submitForm() {
 		if (isSignUp) {
-			fetchBackend('/auth/register', {
+			let resl = await fetchBackend('/auth/register', {
 				method: 'POST',
 				headers: {
-					'Content-Type': 'application/json',
+					'Content-Type': 'application/json'
 				},
-				body: JSON.stringify(formData),
+				body: JSON.stringify(formData)
 			});
+			if (resl.status === 200) {
+				console.log(resl);
+				signInResponse = 'Sign up successful';
+				const tok = await resl.text();
+				console.log(tok);
+				if (tok.length == 16) {
+					token.set(await resl.text());
+				}
+			} else {
+				signInResponse = 'Sign up failed';
+			}
 		} else {
 			fetchBackend('/auth/login', {
 				method: 'POST',
 				headers: {
-					'Content-Type': 'application/json',
+					'Content-Type': 'application/json'
 				},
-				body: JSON.stringify(formData),
+				body: JSON.stringify(formData)
 			}).then((res) => {
 				if (res.status === 200) {
 					console.log(res);
+					signInResponse = 'Sign in successful';
+				} else {
+					signInResponse = 'Sign in failed';
 				}
 			});
 		}
 	}
 </script>
+
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
 <form on:submit|preventDefault={() => submitForm()}>
 	<label for="username">Login</label>
-	<input type="text" name="username" id="username" placeholder="saziz" bind:value={formData.username} />
+	<input
+		type="text"
+		name="username"
+		id="username"
+		placeholder="saziz"
+		bind:value={formData.username}
+	/>
 	<label for="password">Password</label>
-	<input type='password' name="password" id="password" placeholder="ilovehomework123" bind:value={formData.password}/> 
-	<button type="submit" on:click={() => isSignUp = false}>Login</button>
-	<button type="submit" on:click={() => isSignUp = true}>Sign Up</button>
+	<input
+		type="password"
+		name="password"
+		id="password"
+		placeholder="ilovehomework123"
+		bind:value={formData.password}
+	/>
+	<button type="submit" on:click={() => (isSignUp = false)}>Login</button>
+	<button type="submit" on:click={() => (isSignUp = true)}>Sign Up</button>
 </form>
-{#await  fetchData('/auth/info?username=test')}
-	<p>loading...</p>	
-{:then f} 
-	<p>{f}</p>
+<p>{signInResponse}</p>
+{#await fetchData('/auth/info?token=' + $token)}
+	<p>loading...</p>
+{:then f}
+	{#if f.user_id}
+		<p>User ID: {f.user_id} - Hello, {f.username}!</p>
+	{/if}
 {:catch error}
 	<p style="color: red">{error}</p>
 {/await}
+
 <style>
 	* {
 		transition: all;
