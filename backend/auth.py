0792bf5bae6dfd4d6c5a6e349b20817a7a90d60a
Leo Dalgado-Molloy <leodalgado@gmail.com>
2023-05-11 08:26:11 +0000

    move to token based system

--- backend/auth.py
+++ backend/auth.py
@@ -1,5 +1,6 @@
 """Handles safely storing and checking user passwords, and the User class"""
 import random
+import string
 from typing import Union
 
 import bcrypt
@@ -13,13 +14,32 @@ def get_hashed_password(plain_text_password: str):
     return bcrypt.hashpw(plain_text_password.encode("utf-8"), bcrypt.gensalt())
 
 
-def login(username: str, password: str) -> bool:
+def get_user_info(token: str) -> Union[dict, bool]:
+    for user in users:
+        for u in user.session_tokens:
+            print(u)
+        if token in user.session_tokens:
+            return {
+                "username": user.username,
+                "user_id": user.user_id,
+                "logged_in": user.logged_in,
+            }
+    return False
+
+
+def login(username: str, password: str) -> Union[str, bool]:
     """logs in a user with the given username and password"""
     for user in users:
         if user == username:
             if user.check_password(password):
                 user.logged_in = True
-                return True
+                # generate a session token
+                token = "".join(
+                    random.choices(string.ascii_letters + string.digits, k=16)
+                )
+                user.session_tokens.append(token)
+                print(user.session_tokens)
+                return token
             return False
     return False
 
@@ -33,6 +53,7 @@ class User:
         self.username = username
         self.password_hash = get_hashed_password(password)
         self.logged_in = False
+        self.session_tokens = []
         users.append(self)
 
     def __eq__(self, other: Union[str, "User"]) -> bool:
@@ -47,17 +68,6 @@ class User:
             f"User(username: {self.username}, password (hashed): {self.password_hash})"
         )
 
-    def get_user_info(self) -> dict:
-        """Gets all stored user info on the class
-
-        Returns:
-            dict: a dictionary containing all stored user info - user_id, username, password_hash
-        """
-        return {
-            "user_id": self.user_id,
-            "username": self.username,
-        }
-
     def check_password(self, password: str) -> bool:
         """Checks if the given password matches the stored password hash
 
@@ -71,7 +81,8 @@ class User:
 
     @staticmethod
     def generate_random_id() -> int:
-        """generate a random  digit user id number, can be any number, generate 8 random numbers and stitch them together.
+        """generate a random  digit user id number, can be any number,
+        generate 8 random numbers and stitch them together.
         when implemented, check against user db
 
         Returns:
--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,10 @@
 """Main REST API"""
 from typing import Union
 
-from pydantic import BaseModel
-
 import auth
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from fastapi.middleware.cors import CORSMiddleware
-
+from pydantic import BaseModel
 app = FastAPI()
 
 app.add_middleware(
@@ -15,19 +13,22 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-) 
+)
 
 datav = []
+
+
 class Data(BaseModel):
     username: str
     password: str
+
 
 @app.get("/api")
 def read_root():
     return datav
 
 
-@app.get("/api/add")
+@app.get("/api/add", status_code=201)
 def add_new_data(data):
     """adds data in request to datav"""
     datav.append(data)
@@ -50,28 +51,54 @@ def pop_data():
 
 
 # user authentication
-@app.post("/api/auth/register")
-def register(data: Data) -> bool:
+@app.post("/api/auth/register", status_code=201)
+def register(data: Data, response: Response) -> Union[str, bool]:
     """registers a user with the given username and password"""
+    # check both username and password are not empty
+    if data.username == "" or data.password == "":
+        response.status_code = 400
+        return "Username or password cannot be empty"
+    # check if username is already taken
+    for user in auth.users:
+        if user == data.username:
+            response.status_code = 400
+            return "Username already taken"
+    # register user
     auth.User(data.username, data.password)
-    return True
+    token = auth.login(
+        data.username, data.password
+    )  # maybe have this return a cookie instead of a bool
+    return token
 
 
 @app.post("/api/auth/login")
-def login(data: Data) -> Union[dict, bool]:
+def login(data: Data) -> Union[str, bool]:
     """logs in a user with the given username and password"""
-    user = auth.login(data.username, data.password) # maybe have this return a cookie instead of a bool
-    if user:
-        return user.get_user_info()
-    return False
-# i don't know how to handle a 'session'. so for now, handle it on the frontend, 
+    return auth.login(
+        data.username, data.password
+    )  # maybe have this return a cookie instead of a bool
+
+
+# i don't know how to handle a 'session'. so for now, handle it on the frontend,
 # and they can communicate with the backend to check if they're logged in or not using a cookie or something
 
+
 @app.get("/api/auth/info")
-def get_user_info(username: str) -> Union[dict, bool]:
+def get_user_info(token: str, response: Response) -> Union[dict, bool]:
     """gets the user info for the given username"""
-    # search for user with the given username
-    for user in auth.users:
-        if user == username and user.logged_in:
-            return user.get_user_info()
+    # search for user with the given username,
+    # remove first and last character from token because of quotes
+    token = token[1:-1]
+    print(token)
+    info = auth.get_user_info(token)
+    if info:
+        return info
+    response.status_code = 403
     return False
+
+@app.get("/api/auth/dev/clear")
+def clear_users():
+    """clears all users - for development purposes only"""
+    auth.users.clear()
+    return True
+
