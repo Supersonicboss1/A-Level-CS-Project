8b9517a01abe5861c4b687224e581eb3a9cd6641
Leo Dalgado-Molloy <leodalgado@gmail.com>
2023-09-07 21:36:51 +0000

    login cleanup

--- frontend/src/routes/auth/signup/+page.svelte
+++ frontend/src/routes/auth/signup/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	let signInResponse = '';
-	let confirmPassword = '';
+	let response = '';
+	let success = true;
 	let DoBString = '';
 	$: formData.dob = new Date(DoBString).getTime().toString();
-
+	let confirmPassword = '';
 	let formData = {
 		password: '',
 		email: '',
@@ -11,50 +11,13 @@
 		lastName: '',
 		dob: ''
 	};
-	function validateDate() {
-		// get current date in milliseconds
-		const currentYear = new Date().getTime();
-		// get date of birth in milliseconds
-		const dob = new Date(DoBString).getTime();
-		// get difference between current year and year of birth
-		const age = currentYear - dob;
-		// convert difference to years
-		const ageInYears = age / 31536000000; // 31536000000 is the number of milliseconds in a year (probably)
-		// if age is less than 16
-		if (ageInYears < 16) {
-			return false;
-		} else {
-			return true;
-		}
-	}
-	$: console.log(formData);
-	function validateForm() {
-		if (validateDate() === false) {
-			// if under 16
-			signInResponse = 'You must be at least 16 years old to use this service';
-		} else if (formData.password.length < 8) {
-			signInResponse = 'Password must be at least 8 characters';
-		} else if (formData.password !== confirmPassword) {
-			signInResponse = 'Passwords do not match';
-		} else if (formData.email.includes('@') === false) {
-			signInResponse = 'Please enter a valid email address';
-		} else if (formData.firstName === '' || formData.lastName === '') {
-			signInResponse = 'Please enter your full name';
-		} else {
-			signInResponse = '';
-		}
-	}
-
-	function submitForm() {
-		// fetch register
-	}
 </script>
 
 <svelte:head>
 	<title>Login</title>
 	<meta name="description" content="Login to the app" />
 </svelte:head>
-<form on:input={() => validateForm()} on:submit|preventDefault={() => submitForm()}>
+<form>
 	<h2>Sign Up</h2>
 	<sup>or <a href="/auth/login">log in</a></sup>
 	<label for="namegrid">Name</label>
@@ -103,10 +66,8 @@
 		autocomplete="new-password"
 		bind:value={confirmPassword}
 	/>
-	<button type="submit" on:click={() => submitForm()} disabled={signInResponse != ''}
-		>Sign Up</button
-	>
-	{#if signInResponse == '' || signInResponse == 'Success'}
+	<button type="submit">Sign Up</button>
+	{#if success}
 		<!-- do nothing -->
 	{:else}
 		<div class="error">
@@ -116,7 +77,7 @@
 					d="M12 17q.425 0 .713-.288T13 16q0-.425-.288-.713T12 15q-.425 0-.713.288T11 16q0 .425.288.713T12 17Zm0-4q.425 0 .713-.288T13 12V8q0-.425-.288-.713T12 7q-.425 0-.713.288T11 8v4q0 .425.288.713T12 13Zm0 9q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22Zm0-2q3.35 0 5.675-2.325T20 12q0-3.35-2.325-5.675T12 4Q8.65 4 6.325 6.325T4 12q0 3.35 2.325 5.675T12 20Zm0-8Z"
 				/></svg
 			>
-			{signInResponse}
+			{response}
 		</div>
 	{/if}
 </form>
