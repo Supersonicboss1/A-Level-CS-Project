a2451cd4a5424370ff6a52d7da382578e842947c
Leo Dalgado-Molloy <leodalgado@gmail.com>
2023-09-07 21:28:30 +0000

    code cleanup

--- frontend/src/routes/auth/signup/+page.svelte
+++ frontend/src/routes/auth/signup/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { DefaultService } from '$lib/api/index.js';
 	let signInResponse = '';
 	let confirmPassword = '';
 	let DoBString = '';
@@ -20,7 +19,7 @@
 		// get difference between current year and year of birth
 		const age = currentYear - dob;
 		// convert difference to years
-		const ageInYears = age / 31536000000;
+		const ageInYears = age / 31536000000; // 31536000000 is the number of milliseconds in a year (probably)
 		// if age is less than 16
 		if (ageInYears < 16) {
 			return false;
@@ -47,13 +46,7 @@
 	}
 
 	function submitForm() {
-		DefaultService.registerApiAuthRegisterPost(formData)
-			.then((res) => {
-				signInResponse = res ? 'Success' : 'Failed';
-			})
-			.catch(() => {
-				signInResponse = 'Sign up failed - does the user already exist?';
-			});
+		// fetch register
 	}
 </script>
 
@@ -63,7 +56,7 @@
 </svelte:head>
 <form on:input={() => validateForm()} on:submit|preventDefault={() => submitForm()}>
 	<h2>Sign Up</h2>
-	<sup>or <a href="/login" title="sign up coming soon to a website near you!">log in</a></sup>
+	<sup>or <a href="/auth/login">log in</a></sup>
 	<label for="namegrid">Name</label>
 	<div class="namegrid">
 		<input
@@ -113,15 +106,19 @@
 	<button type="submit" on:click={() => submitForm()} disabled={signInResponse != ''}
 		>Sign Up</button
 	>
-	<div class="error">
-		<svg xmlns="http://www.w3.org/2000/svg" width="1rem" height="1rem" viewBox="0 0 24 24"
-			><path
-				fill="#FF0000"
-				d="M12 17q.425 0 .713-.288T13 16q0-.425-.288-.713T12 15q-.425 0-.713.288T11 16q0 .425.288.713T12 17Zm0-4q.425 0 .713-.288T13 12V8q0-.425-.288-.713T12 7q-.425 0-.713.288T11 8v4q0 .425.288.713T12 13Zm0 9q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22Zm0-2q3.35 0 5.675-2.325T20 12q0-3.35-2.325-5.675T12 4Q8.65 4 6.325 6.325T4 12q0 3.35 2.325 5.675T12 20Zm0-8Z"
-			/></svg
-		>
-		{signInResponse}
-	</div>
+	{#if signInResponse == '' || signInResponse == 'Success'}
+		<!-- do nothing -->
+	{:else}
+		<div class="error">
+			<svg xmlns="http://www.w3.org/2000/svg" width="1rem" height="1rem" viewBox="0 0 24 24"
+				><path
+					fill="#FF0000"
+					d="M12 17q.425 0 .713-.288T13 16q0-.425-.288-.713T12 15q-.425 0-.713.288T11 16q0 .425.288.713T12 17Zm0-4q.425 0 .713-.288T13 12V8q0-.425-.288-.713T12 7q-.425 0-.713.288T11 8v4q0 .425.288.713T12 13Zm0 9q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22Zm0-2q3.35 0 5.675-2.325T20 12q0-3.35-2.325-5.675T12 4Q8.65 4 6.325 6.325T4 12q0 3.35 2.325 5.675T12 20Zm0-8Z"
+				/></svg
+			>
+			{signInResponse}
+		</div>
+	{/if}
 </form>
 
 <style lang="scss">
