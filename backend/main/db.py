846976882d304692d38068781043951341a77542
Leo Dalgado-Molloy <leodalgado@gmail.com>
2024-03-08 19:06:30 +0000

    rename database file

--- backend/main/db.py
+++ backend/main/db.py
@@ -1,9 +1,8 @@
 from sqlmodel import SQLModel, create_engine  # noqa: F401
 
-
 from . import schemas  # noqa: F401
 
-sqlite_file_name = "db/accounts.sqlite"
+sqlite_file_name = "db/database.sqlite"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 connect_args = {"check_same_thread": False}
