28d11e9245be090c2e427f2c491afb866f23c41c
Leo Dalgado-Molloy <leodalgado@gmail.com>
2024-03-17 16:39:10 +0000

    Refactor file paths and remove the individual user management

--- frontend/src/routes/admin/userdata/users/[id]/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import type { User } from '$lib/types';
-	import type { PageData } from './$types';
-
-	export let data: PageData;
-	if (!data.user) throw new Error('No data');
-	const user: User = data.user;
-</script>
