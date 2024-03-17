fa52d39ca3c03230375a0a458665d40d8eb2c7db
Leo Dalgado-Molloy <leodalgado@gmail.com>
2024-03-17 16:53:22 +0000

    fix bug where the minimum runtime could be negative

--- frontend/src/routes/user/home/schema.ts
+++ frontend/src/routes/user/home/schema.ts
@@ -2,7 +2,13 @@ import { z } from "zod";
 export const recommendFormSchema = z.object({
 	genre: z.string().optional(),
 	ageRating: z.enum(["Any", "U", "PG", "12A", "12", "15", "18"]),
-	runtime: z.string().optional(),
+	runtime: z.string().optional().refine((x) => {
+		if (x == null) {
+			return true;
+		}
+		const num = Number(x);
+		return !Number.isNaN(num) && num > 0;
+		}, "Runtime must be a positive number")
 });
 
 export type RecommendFormSchema = typeof recommendFormSchema;
